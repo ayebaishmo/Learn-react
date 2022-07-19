674764f48c295f0f8d2a55f50b1b7384d972be30
ayebaishmo <ishmo255super@gmail.com>
2022-07-19 16:31:57 +0600

    Add Jsx

---
+++ src/Jsx.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const user = {
+ firstName: 'Ishmael',
+ lastname: 'Ayebazibwe'
+};
+
+function Jsx() {
+  return user.firstName + ' ' +user.lastname;
+}
+
+export default Jsx;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Jsx from './Jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Jsx />
   </React.StrictMode>
 );
 
